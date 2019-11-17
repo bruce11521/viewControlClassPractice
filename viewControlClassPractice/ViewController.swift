504e31f220d24d992bd6ac49fe1564427da364e9
bruce11521 <bruce11521@gmail.com>
2019-11-17 17:20:10 +0800

    Initial Commit

---
+++ viewControlClassPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewControlClassPractice
+//
+//  Created by 林伯翰 on 2019/11/17.
+//  Copyright © 2019 Bruce Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
